411bacf6892e4bd4f2e57283727c06b4fca0dda5
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-05-20 21:56:45 -0300

    leyendo las categorias de la REST API

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -6,5 +6,13 @@ class EventBrite {
     //Obtiene las categorias en inigt()
     async obtenerCategorias(){
         //Consultar las categorias a la REST API de Eventbrite
+        const respuestaCategorias = await fetch (`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`);
+        //Resperar la respuesta de las categorias y devolverlas en JSON
+        const categorias = await respuestaCategorias.json();
+
+        //devolvemos el resultado
+        return {
+            categorias
+        }
     }
 }
--- js/interfaz.js
+++ js/interfaz.js
@@ -9,10 +9,22 @@ class Interfaz {
 
     //Metodo para cuando inicialice la app
     init(){
-
+        this.imprimirCategorias();
     }
     //Imprimir categorias
     imprimirCategorias(){
-        const listaCategorias = eventbrite.obtenerCategorias();
+        const listaCategorias = eventbrite.obtenerCategorias()
+        .then(categorias => {
+            const cats = categorias.categorias.categories;
+            //seleccionar el select de las categorias
+            const selectCategoria = document.getElementById('listado-categorias');
+            //recorremos el arreglo e imprimimos los <option>
+            cats.forEach(cat => {
+                const option = document.createElement('option');
+                option.value = cat.id;
+                option.appendChild(document.createTextNode(cat.name_localized));
+                selectCategoria.appendChild(option);
+            })
+        })
     }
 }
