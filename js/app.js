07e7344b1deead483187ae9bb4e652d1032aefa9
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-05-20 23:15:08 -0300

    leyendo los datos del formulario

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 //Instanciar clases
 const eventbrite = new EventBrite();
 const ui = new Interfaz();
+
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //Leer el texto del input buscar
+    const textoBuscador = document.getElementById('evento').value;
+
+    //leer el select
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+    //Validando input
+    if(textoBuscador !== ''){
+        console.log('Buscando...');
+    }else {
+        console.log('No hay nada');
+    }
+})
